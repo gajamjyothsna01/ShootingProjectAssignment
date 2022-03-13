371dbeb45d7cb1dec32365eedd4dd86491e99cf4
Gajam_Jyothsna <jyothsna.gajam@yesgnome.com>
2022-03-13 19:58:19 +0530

    Enemy Rotation and destroying after it escapes from GameWindow.

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -18,6 +18,18 @@ public class EnemyMovement : MonoBehaviour
        
         
         transform.Translate(Vector3.left * enemySpeed*Time.deltaTime) ;
+        transform.Rotate(2f, 0f, 0f, Space.Self) ;
 
+        /*if(transform.position.x < -8.7f)
+        {
+            Destroy(gameObject);
+        }*/
+
+    }
+    //This method works for 2D method. This method tells that Once the Enemy is out of the game view,
+    //enemy is automatically invisible with this method.
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
